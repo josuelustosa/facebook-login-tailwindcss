0f4eb44672bcb8571fd7841d832090513728009f
Josué Lustosa <josuelustosa.job@gmail.com>
2022-12-02 11:41:34 -0400

    :construction: defining layout and customing text

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     extend: {
       colors: {
         "blue-facebook": "#1877f2",
+        "gray-facebook": "#f0f2f5",
       },
+      spacing: {
+        "500": "580px"
+      }
     },
   },
   plugins: [],
